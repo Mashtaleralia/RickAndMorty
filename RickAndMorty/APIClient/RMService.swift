7d71c485bdd9853f6679e0a9f3534f8ca90a3c9d
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-08-05 15:53:00 +0300

    API caller

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized constructor
+    private init() {}
+    
+    /// Send Rick and Morty api call
+    /// - Parameters:
+    ///   - execute: Request instance
+    ///   - completion: Callback with data or error
+    public func execute(_ execute: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
