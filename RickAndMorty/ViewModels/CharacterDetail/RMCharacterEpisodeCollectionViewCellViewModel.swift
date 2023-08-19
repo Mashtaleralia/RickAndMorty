b16bb19c4eeaa20e4c3aa1fce1347e1a76df105f
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-08-19 16:34:04 +0300

    fetching and configuring image

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -8,5 +8,8 @@
 import UIKit
 
 final class RMCharacterEpisodeCollectionViewCellViewModel {
-    init() {}
+    private let episodeDataUrl: URL?
+    init(episodeDataUrl: URL?) {
+        self.episodeDataUrl = episodeDataUrl
+    }
 }
