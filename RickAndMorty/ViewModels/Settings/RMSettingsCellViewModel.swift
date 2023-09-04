1bb16a4e3dffb5f78d708334d7ff59f5b922f949
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-09-04 08:30:26 +0300

    create rmsettings vc

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RMSettingsCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Admin on 01.09.2023.
+//
+
+import Foundation
+
+struct RMSettingsCellViewModel {
+    init() {
+         
+    }
+}
